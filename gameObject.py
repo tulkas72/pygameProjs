458a0d39c204cbe4ed02ac835dacd058e1c72179
José M. Sánchez <tulkas72@gmail.com>
2021-04-07 00:34:09 +0200

    Creamos la primera ventana y el bucle del juego que por ahora sólo recoge eventos de terminación

--- gameObject.py
+++ gameObject.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+# la siguiente línea no es necesaria en python 3
 # -*- coding: utf-8 -*-
 
 # Módulos
+import os
+import sys
 import pygame
 from pygame.locals import *
 
 # Constantes
+WIDTH = 640
+HEIGHT = 480
 
 # Clases
 # ---------------------------------------------------------------------
@@ -19,9 +24,17 @@ from pygame.locals import *
 
 
 def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Pruebas Pygame")
+    while True:
+        for eventos in pygame.event.get():
+            if eventos.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
     return 0
 
 
 if __name__ == '__main__':
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
     pygame.init()
     main()
